e8d256d0fd3ac54e05e186cf2ba4db76f037528a
James Patrick Keegan <atomicbomber.email@gmail.com>
2021-02-16 15:47:10 +0700

    Updates.

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const multer = require("multer")
+const fs = require("fs")
+const mammoth = require("mammoth")
+const stream = require("stream")
+const HtmlToDocx = require("html-to-docx")
+
+require("dotenv").config()
+
+const app = express()
+const upload = multer({dest: `${__dirname}/uploads`})
+
+app.post("/word-to-html", upload.single("file"), function (req, res) {
+    mammoth.convertToHtml({path: req.file.path})
+        .then(result => {
+            res.send(result.value)
+        })
+        .catch(() => {
+            res.status(400)
+            res.send("Error: failed to process file.")
+        })
+})
+
+app.post("/html-to-word", upload.single("file"), async function (req, res) {
+    let converted = await HtmlToDocx(
+        fs.readFileSync(req.file.path).toString()
+    )
+
+    let readStream = new stream.PassThrough()
+    readStream.end(converted)
+
+    res.attachment(`${req.file.originalname}.docx`)
+    readStream.pipe(res)
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started at port ${process.env.PORT}.`)
+})
